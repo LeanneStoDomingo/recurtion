73046d9c51801d6304f7bb0b9e296a149cf93db2
Leanne Sto. Domingo <leanneasd@gmail.com>
2021-05-19 15:25:01 -0400

    Add function to get recurring tasks

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Client } = require('@notionhq/client');
+require('dotenv').config();
+
+// Initializing a client
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+});
+
+const getRecurringTasks = async () => {
+    const res = await notion.search();
+    const allTasks = res.results.filter(item => item.object === 'page');
+    const recurringTasks = allTasks.filter(task => task.properties['Recur Interval'].rich_text.length !== 0);
+
+    return recurringTasks.map(task => {
+        return {
+            id: task.id,
+            done: task.properties.Done.checkbox,
+            recurInterval: task.properties['Recur Interval'].rich_text[0].plain_text
+        }
+    });
+}
+
+
+// Main function
+(async () => {
+    const tasks = await getRecurringTasks();
+    console.log(tasks)
+})()
