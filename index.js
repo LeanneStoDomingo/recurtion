680cd2cf0d5b70a9a694304b68fd984023f7c187
Leanne Sto. Domingo <leanneasd@gmail.com>
2021-05-19 15:51:02 -0400

    Include start date in list of recurring tasks and flip filtering by Done

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const getRecurringTasks = async () => {
     const res = await notion.search();
     const allTasks = res.results.filter(item => item.object === 'page');
     const recurringTasks = allTasks.filter(task => {
-        return !task.properties.Done.checkbox && task.properties['Recur Interval'].rich_text.length !== 0;
+        return task.properties.Done.checkbox && task.properties['Recur Interval'].rich_text.length !== 0;
     });
 
     return recurringTasks.map(task => {
         return {
             id: task.id,
+            date: task.properties['Due Date'].date.start,
             recurInterval: task.properties['Recur Interval'].rich_text[0].plain_text
         }
     });
