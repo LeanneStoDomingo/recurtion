dac967a42382a0bf83dca757ff5a38bbe1ee8f4d
Leanne Sto. Domingo <leanneasd@gmail.com>
2021-06-06 22:20:22 -0400

    Set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
 import { Login, Signup, Dashboard } from './pages'
 
 const App = () => {
-  return (
-    <>
-      <Login />
-      <Signup />
-      <Dashboard />
-    </>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/dashboard' component={Dashboard} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/signup' component={Signup} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
