cfdbaf2a32d234222192bd722f48d60b6b6eab73
Leanne Sto. Domingo <leanneasd@gmail.com>
2021-06-11 16:13:14 -0400

    Add styling to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
     return (
-        <div>
-            Home
+        <div className='flex flex-col items-center text-center'>
+            <h1>Recurtion<sup>beta</sup></h1>
+
+            <div>
+                Automatically adds recurring tasks to Notion using its brand new API!
+                This project is not maintained or affiliated with Notion
+            </div>
+
+            <div className='flex'>
+                <Link to='/login'>Log In</Link>
+                <Link to='/signup'>Sign Up</Link>
+            </div>
         </div>
     )
 }
